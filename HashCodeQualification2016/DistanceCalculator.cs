fe4c891a8d1bf2a342ba9c113b52203b7402cb88
Luis Reis <luiscubal@gmail.com>
2016-02-11 18:57:08 +0000

    Add distance calculator

---
+++ HashCodeQualification2016/DistanceCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeQualification2016
+{
+    public static class DistanceCalculator
+    {
+        public static int CalculateDistance(Position p1, Position p2)
+        {
+            int rowDiff = p1.Row - p2.Row;
+            int colDiff = p1.Column - p2.Column;
+
+            return (int)Math.Ceiling(Math.Sqrt(rowDiff*rowDiff + colDiff*colDiff));
+        }
+    }
+}
