2a91401907e69ba94cf1deb1c4fca2b06ba0b622
Luis Reis <luiscubal@gmail.com>
2016-02-11 19:31:53 +0000

    Draft

---
+++ HashCodeQualification2016/Drone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeQualification2016
+{
+    class Drone
+    {
+        public int Id;
+
+        Position NextPosition;
+        int TurnsToNextAction;
+
+        public Drone(int id, Position initialPosition)
+        {
+            Id = id;
+            NextPosition = initialPosition;
+        }
+
+        public bool IsAvailable
+            => TurnsToNextAction == 0;
+
+        internal void ExecuteTurn(ProblemDescription description, List<Command> commands)
+        {
+            if (TurnsToNextAction > 0)
+            {
+                --TurnsToNextAction;
+            } else
+            {
+                var bestOrder = GetBestOrder(description, NextPosition);
+
+                if (bestOrder != null)
+                {
+                    ExecuteOrder(description, bestOrder.Value);
+                }
+            }
+        }
+
+        public int? GetBestOrder(ProblemDescription description, Position nextPosition)
+        {
+            return null;
+        }
+
+        public void ExecuteOrder(ProblemDescription description, int i)
+        {
+            var order = description.Orders[i];
+            description.Orders.RemoveAt(i);
+
+            // TODO
+        }
+    }
+}
---
+++ HashCodeQualification2016/Solver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeQualification2016
+{
+    public static class Solver
+    {
+        public static List<Command> Execute(ProblemDescription description)
+        {
+            var commands = new List<Command>();
+            var drones = new List<Drone>();
+            for (int i = 0; i < description.NumDrones; ++i)
+            {
+                drones.Add(new Drone(i, description.Warehouses[0].position));
+            }
+
+            for (int turnId = 0; turnId < description.Deadline; ++turnId)
+            {
+                ExecuteTurn(description, drones, commands);
+            }
+
+            return commands;
+        }
+
+        static void ExecuteTurn(ProblemDescription description, List<Drone> drones, List<Command> commands)
+        {
+            foreach (var drone in drones)
+            {
+                drone.ExecuteTurn(description, commands);
+            }
+        }
+    }
+}
